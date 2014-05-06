3a308505ace244f8dc1530b1f42cbe186cdc3582
Christian Kyony <ckyony@changamuka.com>
2014-05-06 15:29:02 +0200

    Restore the gap at the top of the window

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,5 +5,22 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myTerminal = "/usr/bin/urxvt"
+myModMask = mod1Mask
+
 main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar"
     xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , terminal = myTerminal
+        , modMask = myModMask
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
