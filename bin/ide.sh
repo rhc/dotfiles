72206b2b0789328d4043eee3a655499fa9e436e9
Christian Kyony <ckyony@changamuka.com>
2014-10-14 17:21:41 +0200

    Before xmonad-gnome3

--- bin/ide.sh
+++ bin/ide.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-terminator -e "rails server" &
-terminator -e "bundle exec guard" &
 terminator -e "vim Gemfile" &
+terminator -e "bundle exec guard" &
+terminator -e "rails server" &
 
