52a74bd0903d7336bcd979cd2587a28861ba8f55
Christian Kyony <ckyony@changamuka.com>
2014-11-16 16:41:48 +0200

    Add helper methods to select date and time in Capybara tests

--- rails/custom/test/test_helper.rb
+++ rails/custom/test/test_helper.rb
@@ -35,6 +35,22 @@ class ActiveSupport::TestCase
   end
 
   # Add more helper methods to be used by all tests here...
+  #todo: improve this part
+  def select_date(date, options = {})
+    field = options[:from]
+    select date.strftime('%Y'), :from => "#{field}_1i" #year
+    select date.strftime('%B'), :from => "#{field}_2i" #month
+    select date.strftime('%d'), :from => "#{field}_3i" #day 
+  end
+
+  def select_date_and_time(date, options = {})
+    field = options[:from]
+    select date.strftime('%Y'), :from => "#{field}_1i" #year
+    select date.strftime('%B'), :from => "#{field}_2i" #month
+    select date.strftime('%d'), :from => "#{field}_3i" #day 
+    select date.strftime('%H'), :from => "#{field}_4i" #hour
+    select date.strftime('%M'), :from => "#{field}_5i" #minute
+  end
 
 end
 
