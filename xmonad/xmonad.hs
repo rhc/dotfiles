98c8767f9cea9d49998998b2135bb8dde9b9e191
Christian Kyony <ckyony@changamuka.com>
2015-05-07 15:32:10 +0200

    Initial config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,15 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Layout.NoBorders
+
 
 myTerminal = "urxvt"
+myLayout = tiled ||| Full
+  where 
+    tiled = Tall nmaster delta ratio
+    nmaster = 1 -- default number of windows in the master pane
+    ratio = 1/2 -- default proportion of screen occupied by master pane
+    delta = 3/100 -- increment when resizing panes
 
 main = xmonad gnomeConfig 
   {
